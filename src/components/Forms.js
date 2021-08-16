6c4a38a221e3fec0f9b3394135c988efcf15c251
tonyishangu <tonyishangu@gmail.com>
2021-08-16 23:05:38 +0300

    implement handle change function for the form

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
-const Forms = () => {
+const Forms = (props) => {
+    const [input, setInput] = useState(props.edit ? props.edit.value : '')
+
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        inputRef.current.focus()
+    })
+
+    const handleChange = e => {
+        
+    }
     return (
         <div>
             
